4454532d627fe7794c4e3535fdc1cc1a3b057940
Xinnji <xinnji@gmail.com>
2021-08-10 17:34:10 -0700

    Maybe this is how I should handle mobs? One thread in the server which updates the world dict?

--- game.py
+++ game.py
@@ -10,13 +10,17 @@ saving clients' sockets and message history.
 from json import dumps, loads
 
 
-def loadworld(self, file):
+def loadworld(file):
     """Load a static world file into a dict."""
     world = open(file, "r").read()
     return loads(world)
 
 
-def saveworld(self, file, w_dict):
+def saveworld(file, w_dict):
     """Save an active world dict into a JSON world file."""
     with open(file, 'w') as file:
         file.write(dumps(w_dict))
+
+
+def handlemobs(mob):
+    """Execute the events for each mob in the world in sequence."""
