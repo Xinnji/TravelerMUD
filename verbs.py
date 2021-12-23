d73a878810d8bd114ad6b524d20df032da381f61
Xinnji <xinnji@gmail.com>
2021-12-22 20:44:54 -0800

    Adding to new branch.
    Verbs are the text commands executed by players through the parse event, thus allowing interactivity with the game world.

--- verbs.py
+++ verbs.py
@@ -5,14 +5,30 @@ author: Jason Scott-Hakanson
 In game commands.
 
 Arguments:
-id -- the unique identifier of the player
+playerid -- the unique identifier of the player
 args -- argument string to be parsed by the verb
 
 Commands that can be executed by the user as a player. Largely return
 messages to be appended to the user's message box and sent to the client. Many
-also edit the world file in some way before doing so.
+also change the player or world in some way before doing so.
 """
 
 
-def go(id, args):
-    """Move the player from a room to an adjacent one."""
+def go(playerid, args):
+    """Change the player's location to a connected room."""
+
+
+def look(playerid, args):
+    """Print the description of the player's location, or an entity."""
+
+
+def do(playerid, args):
+    """Display an emoted message to all entities in the same room."""
+
+
+def say(playerid, args):
+    """Display a text message to all entities in the same room."""
+
+
+def cast(playerid, args):
+    """Allows a player of sufficient power to cast magical spells."""
