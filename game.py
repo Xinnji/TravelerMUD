121e821fb4f650bb0ed310b0044979d7bc41ebbd
Xinnji <xinnji@gmail.com>
2021-12-22 19:58:21 -0800

    Adding to new branch.
    Game object handles setup and teardown of travelermud game instances.

--- game.py
+++ game.py
@@ -2,35 +2,80 @@
 file: game.py
 author: Jason Scott-Hakanson
 
-Start the game world.
-
-Load in the game world from a JSON "world file." Initialize a list for
-saving clients' sockets and message history.
+Functions for starting, stopping and handling an instance of Travelermud.
 """
-from json import dumps, loads
+import sqlite3
 
 
-def loadworld(file):
-    """Load a static world file into a dict."""
-    world = open(file, "r").read()
-    return loads(world)
+class Game:
+    def __init__(self, worldfile):
+        """Load the SQLite database to be used for the game."""
+        self.worldfile = worldfile
+        self.clients = []
+        with sqlite3.connect(worldfile) as con:
+            con.execute(
+                '''create table if not exists Room(
+                id       integer not null unique primary key,
+                name     text    not null,
+                capacity integer not null default -1,
+                event    text,
+                desc     text    not null);''')
+            con.execute(
+                '''create table if not exists Exit(
+                exitid   integer not null,
+                name     text    not null,
+                enterid  integer not null,
+                event    text,
+                foreign key(enterid) references Room(id),
+                foreign key(exitid)  references Room(id),
+                unique(enterid, name));''')
+            con.execute(
+                '''create table if not exists Entity(
+                id       integer not null unique primary key,
+                name     text    not null,
+                stamina  integer,
+                power    integer not null default 0,
+                size     integer not null default 0,
+                capacity integer not null default 0,
+                location integer,
+                event    text,
+                intro    text    not null,
+                desc     text    not null,
+                foreign key(location) references Room(id));''')
+            con.execute(
+                '''create table if not exists Player(
+                id       integer not null unique primary key,
+                username text    not null unique,
+                password text    not null,
+                name     text    not null,
+                stamina  integer          default 20,
+                power    integer not null default 0,
+                size     integer not null default 20,
+                capacity integer not null default 10,
+                location integer,
+                event    text             default "parse",
+                intro    text    not null,
+                desc     text    not null,
+                foreign key(location) references Room(id));''')
+        print(f"Loaded '{worldfile}' successfully.")
 
+    def query(self, body):
+        """Execute a SQLite query on the loaded database."""
+        with sqlite3.connect(self.worldfile) as con:
+            cur = con.execute(body)
+        return cur
 
-def saveworld(file, w_dict):
-    """Save an active world dict into a JSON world file."""
-    with open(file, 'w') as file:
-        file.write(dumps(w_dict))
+    def run(self):
+        """Start an instance of Travelermud."""
 
+    def stop(self):
+        """Shut down an active instance of Travelermud."""
 
-def handlemobs(mob):
-    """Execute the events for each mob in the world in sequence."""
+    def login(self, client):
+        """Log the client in to an existing character, or register a new one.
 
+        Arguments:
+        client -- the client connection socket"""
 
-
-# Keep a dictionary of registered characters in the world file. This is the
-# database for all player characters that have been created. When a player
-# connects to the game, they are asked to log in. If the username they choose
-# does not exist, it is added as a new character to the registry. Either way,
-# they also enter a password for the character, and then are placed into the
-# world as an online player. Their messages are stored in the online player
-# area of the dict, and only their names are stored in their current rooms.
+    def logout(self, client):
+        """Used when a client disconnects to destroy their entity."""
